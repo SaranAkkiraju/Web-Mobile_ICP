c951c42e284a9e2898c462d2f7951f12fa7160cd
shannu1221 <42659018+shannu1221@users.noreply.github.com>
2019-09-07 15:29:05 -0500

    adding comments

--- ICP3/SourceCode/rock_paper_scissors_game/script.js
+++ ICP3/SourceCode/rock_paper_scissors_game/script.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
     });
 });
 
+//Rock–paper–scissors (also known as scissors–rock–paper or other variants) is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock" (a closed fist), "paper" (a flat hand), and "scissors" (a fist with the index finger and middle finger extended, forming a V). "Scissors" is identical to the two-fingered V sign (also indicating "victory" or "peace") except that it is pointed horizontally instead of being held upright in the air. A simultaneous, zero-sum game, it has only two possible outcomes: a draw, or a win for one player and a loss for the other.
 // This function checks who is the winner
 function compare(choice1) {
     // Computer's choice will be stored as systemChoice which is randomly generated
@@ -22,38 +23,42 @@ function compare(choice1) {
     } else {
         choice2 = "scissors";
     }
-
+    // Checks if user's choice and Systems choice are same
     if (choice1 === choice2) {
 
         alert( "Its a tie!!!" + " " + "Lets play again.");
     }
 
+    // Check if user's choice is rock
     else if (choice1 === "rock") {
-
+        // Check if system's choice is scissors when User's choice is rock
         if (choice2 === "scissors") {
             alert( "rock wins"+ "!!! " + "Your the Winner, nice job!");
         }
+        // Check if system's choice is Paper when User's choice is rock
         else {
             alert("paper wins"  + "!!! " + "Computer Wins, Good luck Next time!!!.") ;
         }
     }
-
+    // Check if user's choice is Paper
     else if (choice1 === "paper") {
-
+        // Check if system's choice is rock when User's choice is paper
         if (choice2 === "rock") {
             alert("paper wins"+ "!!! " + "Your the Winner, nice job!") ;
         }
+        // Check if system's choice is Scissors when User's choice is paper
         else {
             alert("scissors wins"+ "!!! " + "Computer Wins, Good luck Next time!!!.") ;
         }
 
     }
-
+    // Check if user's choice is scissors
     else if (choice1 === "scissors") {
-
+        // Check if system's choice is rock when User's choice is Scissors
         if (choice2 === "rock") {
             alert("rock wins"  + " " + "Computer Wins, Good luck Next time!!!.") ;
         }
+        // Check if system's choice is paper when User's choice is Scissors
         else {
             alert("scissors win"  + " " + "Your the Winner, nice job!") ;
         }
