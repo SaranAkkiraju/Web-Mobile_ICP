bf9483dc6e8ee92ae7b94c896e61849f3aeb0d77
shannu1221 <42659018+shannu1221@users.noreply.github.com>
2019-11-23 15:24:02 -0600

    updating code

--- ICP12/SourceCode/SpeechToText/app/src/main/java/com/vijaya/speechtotext/MainActivity.java
+++ ICP12/SourceCode/SpeechToText/app/src/main/java/com/vijaya/speechtotext/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView mVoiceInputTv;
     private ImageButton mSpeakBtn;
+    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+    String txt1="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,27 +30,30 @@ public class MainActivity extends AppCompatActivity {
 
         mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
         mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
+
         mSpeakBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                startVoiceInput();
+                startVoiceInput("hello");
             }
         });
     }
 
-    private void startVoiceInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+    private void startVoiceInput(String txt) {
+        if (txt=="")
+        {
+            txt="hello";
+        }
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Saran");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, txt);
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
         } catch (ActivityNotFoundException a) {
 
         }
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -56,14 +64,68 @@ public class MainActivity extends AppCompatActivity {
                 if (resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     mVoiceInputTv.setText(result.get(0));
-                    if (result.get(0)=="hello")
-                    {
+                    if (result.get(0).equals("hello")) {
+                        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "whats ur name?");
+                        try {
+                            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+                        } catch (ActivityNotFoundException a) {
+
+                        }
+                    }
+
+                        if (result.get(0).equals("rank")) {
+
+                            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "i am not feeling good. what should i do");
+                            try {
+                                startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+                            } catch (ActivityNotFoundException a) {
+
+                            }
+                        }
+                        if (result.get(0).equals("take tablets")) {
+                            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "what medicines should i take?");
+                            try {
+                                startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+                            } catch (ActivityNotFoundException a) {
+
+                            }
+                        }
+
+                    if (result.get(0).equals("nice")) {
+                        txt1="rank";
+                        SimpleDateFormat sdfDate = new SimpleDateFormat("HH:mm");//dd/MM/yyyy
+                        Date now = new Date();
+                        String[] strDate = sdfDate.format(now).split(":");
+                        if(strDate[1].contains("00"))strDate[1] = "o'clock";
+                        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "current time is "+sdfDate.format(now));
+                        try {
+                            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+                        } catch (ActivityNotFoundException a) {
+
+                        }
 
                     }
+
+
+                    }
+
+
+
+
+
+
+
                 }
                 break;
             }
 
         }
     }
-}
